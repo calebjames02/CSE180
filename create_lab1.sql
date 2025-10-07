d4de99c00e873a14b3ffa174cf659fbd8e419def
calebjames02 <calebjamesbergen@gmail.com>
2025-10-06 21:39:10 -0700

    setup database file

---
+++ create_lab1.sql
@@ -0,0 +1,72 @@
+CREATE TABLE Passenger(
+	passengerID 	INT,
+	passengerName 	VARCHAR(40),
+	email 			VARCHAR(40),
+	dateOfBirth 	DATE,
+	frequentFlyer 	CHAR(1)
+	PRIMARY KEY (passengerID)
+);
+
+CREATE TABLE Airport(
+	airportCode CHAR(3),
+	city VARCHAR(40),
+	country VARCHAR(40),
+	numRunways INT,
+	avgDelayMinutes INT
+	PRIMARY KEY (airportCode)
+);
+
+CREATE TABLE Flight(
+	flightID INT,
+	departureAirport CHAR(3),
+	arrivalAirport CHAR(3),
+	scheduledDeparture TIMESTAMP,
+	scheduledArrival TIMESTAMP,
+	actualDeparture TIMESTAMP,
+	actualArrival TIMESTAMP,
+	aircraftType VARCHAR(40)
+	PRIMARY KEY (flightID)
+	FOREIGN KEY (departureAirport) REFERENCES Airport (airportCode)
+	FOREIGN KEY (arrivalAirport) REFERENCES Airport (airportCode)
+);
+
+CREATE TABLE CrewMember(
+	crewID INT,
+	crewName VARCHAR(40),
+	crewRole VARCHAR(40),
+	yearsExperience INT
+	PRIMARY KEY (crewID)
+);
+
+CREATE TABLE Reservation(
+	reservationID INT,
+	passengerID INT,
+	flightID INT,
+	bookingDate DATE,
+	seatClass CHAR(1),
+	ticketPrice NUMERIC(7, 2), -- Check if this is correct
+	paymentMethod VARCHAR (40)
+	PRIMARY KEY (reservationID)
+	FOREIGN KEY (passengerID) REFERENCES Passenger
+	FOREIGN KEY (flightID) REFERENCES Flight
+);
+
+CREATE TABLE CheckIn(
+	reservationID INT,
+	passengerID INT,
+	checkInTime TIMESTAMP,
+	bagCount INT,
+	seatNumber VARCHAR(40)
+	PRIMARY KEY (reservationID, passengerID)
+	FOREIGN KEY (reservationID) REFERENCES Reservation
+	FOREIGN KEY (PassengerID) REFERENCES Passenger
+);
+
+CREATE TABLE FlightCrewAssignment(
+	flightID INT,
+	crewID INT,
+	compensation NUMERIC(7, 2)
+	PRIMARY KEY (flightID, crewID)
+	FOREIGN KEY (flightID) REFERENCES Flight
+	FOREIGN KEY (crewID) REFERENCES CrewMember
+);
