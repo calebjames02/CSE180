fef26c0be3ed6802dc6719b6ae5e4056b9b4d1d2
calebjames02 <calebjamesbergen@gmail.com>
2025-11-12 16:09:36 -0800

    set up foreign keys and general constraints

---
+++ lab3/general.sql
@@ -0,0 +1,9 @@
+ALTER TABLE Airport ADD CONSTRAINT airportRunwaysPositive
+	CHECK (numRunways > 0);
+
+ALTER TABLE Flight ADD CONSTRAINT validAircraftType
+	CHECK (aircraftType IN ('Boeing 737', 'Airbus A320', 'Boeing 777', 'Airbus A380'));
+--	Don't need to check aircraft type being null, since if it is null the statement evaluates to unknown which results in it still passing the test
+
+ALTER TABLE Reservation ADD CONSTRAINT ifNullPriceThenNullMethod
+	CHECK ((ticketPrice IS NULL AND paymentMethod IS NULL) OR (ticketPrice IS NOT NULL AND paymentMethod IS NOT NULL));
