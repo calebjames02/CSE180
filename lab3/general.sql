c631c81b45806a696d0f9074409db80b23a815f6
calebjames02 <calebjamesbergen@gmail.com>
2025-11-17 21:00:19 -0800

    done

--- lab3/general.sql
+++ lab3/general.sql
@@ -6,4 +6,4 @@ ALTER TABLE Flight ADD CONSTRAINT validAircraftType
 --	Don't need to check aircraft type being null, since if it is null the statement evaluates to unknown which results in it still passing the test
 
 ALTER TABLE Reservation ADD CONSTRAINT ifNullPriceThenNullMethod
-	CHECK ((ticketPrice IS NULL AND paymentMethod IS NULL) OR (ticketPrice IS NOT NULL AND paymentMethod IS NOT NULL));
+	CHECK (ticketPrice IS NOT NULL OR (ticketPrice IS NULL and paymentMethod IS NULL));
